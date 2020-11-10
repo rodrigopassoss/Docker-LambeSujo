62a9ff698e48aafa3af814e64d13106e74912c30
rodrigo <iamrodrigopassos@gmail.com>
2020-11-10 00:53:57 -0300

    Atualização 1h madruga

--- rundocker.sh
+++ rundocker.sh
@@ -26,6 +26,6 @@ docker run  -it \
             --volume="/etc/shadow:/etc/shadow:ro" \
             --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
             --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-            $DOCKER_IMAGE
-
+            $DOCKER_IMAGE 
+            
 docker container rm $CONTAINER_NAME -f
